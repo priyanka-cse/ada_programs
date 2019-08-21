214fc997425eeafc1343cc7dd845042a2ac11695
priyanka-cse <53851643+priyanka-cse@users.noreply.github.com>
2019-08-21 12:25:15 +0530

    Update lab-2a.cpp

--- lab-2a.cpp
+++ lab-2a.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 
+#include <iostream>
+
 using namespace std;
 int main()
 {
 	int a[100],n,i,j,k,temp,max;
 	cout<<"enter the size\n";
 	cin>>n;
+	cout<<"enter the elements";
 	for(i=0;i<n;i++)
 		cin>>a[i];
-	cout<<"enter key";
+	cout<<"enter the k";
 	cin>>k;
 	for(i=0;i<k;i++)
 	{
@@ -22,6 +25,6 @@ int main()
 		a[i]=a[max];
 		a[max]=temp;
 	}
-	cout<<"k="<<a[k-1];
+	cout<<"the kth smallest number is:"<<a[k-1];
 	return 0;
 }
