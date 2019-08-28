dae2e3966deaf9d563c609d5280cc0d06077db5d
priyanka-cse <53851643+priyanka-cse@users.noreply.github.com>
2019-08-28 12:33:20 +0530

    Create mergesor.cpp

---
+++ mergesor.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+using namespace std;
+void combine(int a[],int low,int mid,int high)
+{
+	int c[100],i,j,k;;
+	i=low;
+	j=mid+1;
+	k=low;
+	while(i<=mid && j<=high)
+	{
+		if(a[i]<a[j])
+			c[k++]=a[i++];
+		else
+			c[k++]=a[j++];
+	}
+	if(i>mid)
+		while(j<=high)
+			c[k++]=a[j++];
+	if(j>high)
+		while(i<=mid)
+			c[k++]=a[i++];
+	for(i=low;i<=high;i++)
+		a[i]=c[i];
+}
+void split(int a[],int low,int high)
+{
+	int mid;
+	if(low<high)
+	{
+		mid=(low+high)/2;
+		split(a,low,mid);
+		split(a,mid+1,high);
+		combine(a,low,mid,high);
+	}
+}
+int main()
+{
+	int a[100],n,i;
+	cout<<"enter size\n";
+	cin>>n;
+	cout<<"enter elements\n";
+	for(i=0;i<n;i++)
+		cin>>a[i];
+	split(a,0,n);
+	cout<<"sorted elements are\n";
+	for(i=0;i<n;i++)
+		cout<<a[i]<<"\n";
+	return 0;
+}
+	
+	
